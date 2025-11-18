f78f94e0cf763980d80c9e13cd06d610873ad651
Azizbek Sultonmurodov <00023983@wiut.uz>
2025-11-18 09:38:41 +0500

    again

---
+++ models.py
@@ -0,0 +1,9 @@
+
+class Dispatcher():
+    def __init__(self,name,age,department):
+          self.name = name
+          self.age = age
+          self.department = department
+    def __str__(self):
+         return f"{self.name},{self.age}"    
+
